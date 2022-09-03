5c55f2a6a80d44f505b2f621a86ed7111abd3d9b
Hendrek Rodrigues <llhendrek@gmail.com>
2022-09-03 10:59:18 -0300

    Criando filtro por nome e adicionando OpenApi

--- pharmacies/pharmacies/main.py
+++ pharmacies/pharmacies/main.py
@@ -1,30 +1,21 @@
 
 #!/usr/bin/env python3
-from flask import Flask, jsonify
-from flask import Response
-from .ext import database2
+from flask import Flask
 from .blueprint.v1 import resources
-# from .ext import doc_swagger
+from .ext import doc_swagger
 
-from .models.core import PatientsModel, PatientsSchema, PharmaciesSchema, TransactionsModel, TransactionsSchema, UsersModel, PharmaciesModel, UsersSchema
-
-#----------------------------------------------------------------------------#
-# Initialize app and set config
 
 def create_app():
 
     app = Flask(__name__)
-
     app.config['JSON_AS_ASCII'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db = database2.SessionLocal()
-    
     resources.init_app(app)
     # cors.init_app(app)
+    doc_swagger.init_app(app)
 
-    return app 
+    return app
 
-#----------------------------------------------------------------------------#
 
 if __name__ == '__main__':
     create_app.run()
