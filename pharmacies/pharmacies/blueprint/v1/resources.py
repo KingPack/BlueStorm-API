584d7219e2cbe914ab4491414d5c85f12883c286
Lubeen <luan.silvapvh@gmail.com>
2022-09-03 11:21:05 -0400

    filtros de endpoint

--- pharmacies/pharmacies/blueprint/v1/resources.py
+++ pharmacies/pharmacies/blueprint/v1/resources.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from flask import Blueprint, Response, jsonify
 from ...ext.database import SessionLocal
 from ...models.core import *
@@ -21,30 +22,29 @@ def index():
     print(id)
     return Response(result, status=200, mimetype='text/html')
 
-
-@bp.route('/patients/<name>/', methods=['GET'])
+@bp.route('/patients/', methods=['GET'])
 @doc_swagger.swag_from('docs/patients.yaml')
-def patients(name:str):
-    try:
-        if name:
-            query_patient = db.query(PatientsModel).filter(PatientsModel.FIRST_NAME.ilike(f'%{name.upper()}%')).all()
-            schema_patient = PatientsSchema(many=True)
-            json_patient = schema_patient.dump(query_patient)
-            status_code = 200
-            result = json_patient
-        else:
-            query_patients = db.query(PatientsModel).all()
-            json_patients = schema_patients.dump(query_patients)
-            status_code = 200
-            result = json_patients
-            
-    except Exception as error:
-        status_code = 404
-        result = {
-            'mensagem' : error,
-            'status_code': status_code
-        }
+def patients():
+    print('passei')
+    query_patients = db.query(PatientsModel).all()
+    json_patients = schema_patients.dump(query_patients)
+    status_code = 200
+    result = json_patients
+    
+    db.close()
+    return jsonify(result), status_code
 
+@bp.route('/patients/<string:name>/', methods=['GET'])
+@doc_swagger.swag_from('docs/patients.yaml')
+def patients_filter(name):
+
+    print('passei')
+    query_patient = db.query(PatientsModel).filter(PatientsModel.FIRST_NAME.ilike(f'%{name.upper()}%')).all()
+    schema_patient = PatientsSchema(many=True)
+    json_patient = schema_patient.dump(query_patient)
+    status_code = 200
+    result = json_patient
+        
     db.close()
     return jsonify(result), status_code
 
@@ -58,6 +58,17 @@ def pharmacies():
 
     return jsonify(json_pharmacies)
 
+@bp.route('/pharmacies/<string:pharmacie>', methods=['GET'])
+@doc_swagger.swag_from('docs/pharmacies.yaml')
+def pharmacies_filter(pharmacie):
+
+    query_pharmacies = db.query(PharmaciesModel).filter(PharmaciesModel.NAME.ilike(f'%{pharmacie.upper()}%')).all()
+    schema_pharmacies = PharmaciesSchema(many=True)
+    json_pharmacies = schema_pharmacies.dump(query_pharmacies)
+    db.close()
+    return jsonify(json_pharmacies)
+    
+
 
 @bp.route('/transactions/', methods=['GET'])
 @doc_swagger.swag_from('docs/transactions.yaml')
@@ -67,4 +78,14 @@ def transactions():
     json_transactions = schema_transactions.dump(query_transactions)
 
     return jsonify(json_transactions)
+
+@bp.route('/transactions/<string:transaction>', methods=['GET'])
+@doc_swagger.swag_from('docs/transaction.yaml')
+def transaction_filter(transaction):
+
+    query_transaction = db.query(TransactionsModel).filter(TransactionsModel.NAME.ilike(f'%{transaction.upper()}%')).all()
+    schema_transaction = TransactionsSchema(many=True)
+    json_transaction = schema_transactions.dump(query_transaction)
+    db.close()
+    return jsonify(json_transaction)
 
