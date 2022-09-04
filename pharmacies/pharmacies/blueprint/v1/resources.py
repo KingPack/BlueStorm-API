522bc5509635af239d4194cc8de8722ed1dedafc
Hendrek Rodrigues <llhendrek@gmail.com>
2022-09-03 21:55:52 -0300

    Refatoração e adição do flake8

--- pharmacies/pharmacies/blueprint/v1/resources.py
+++ pharmacies/pharmacies/blueprint/v1/resources.py
@@ -1,91 +1,93 @@
-from unicodedata import name
-from flask import Blueprint, Response, jsonify
+from flask import Blueprint
+from flask import Response
+from flask import jsonify
+
 from ...ext.database import SessionLocal
-from ...models.core import *
-from ...ext import doc_swagger
+from ...ext.doc_swagger import swag_from
+
+from ...models.core import PatientsModel
+from ...models.core import PatientsSchema
+from ...models.core import PharmaciesModel
+from ...models.core import PharmaciesSchema
+from ...models.core import TransactionsModel
+from ...models.core import TransactionsSchema
+
 
 bp = Blueprint('bluestorm_v1', __name__, url_prefix='/v1/')
 db = SessionLocal()
 
 schema_patients = PatientsSchema(many=True)
 schema_pharmacies = PharmaciesSchema(many=True)
-schema_users = UsersSchema(many=True)
 schema_transactions = TransactionsSchema(many=True)
+
 
 def init_app(app):
     app.register_blueprint(bp)
-    
+
 
 @bp.route('/', methods=['GET'])
 def index():
     result = '<H1> Pagina Inicial</H1>'
-    print(id)
     return Response(result, status=200, mimetype='text/html')
 
+
 @bp.route('/patients/', methods=['GET'])
-@doc_swagger.swag_from('docs/patients.yaml')
+@swag_from('docs/patients.yaml')
 def patients():
-    print('passei')
     query_patients = db.query(PatientsModel).all()
     json_patients = schema_patients.dump(query_patients)
     status_code = 200
-    result = json_patients
-    
     db.close()
-    return jsonify(result), status_code
+    return jsonify(json_patients), status_code
+
 
 @bp.route('/patients/<string:name>/', methods=['GET'])
-@doc_swagger.swag_from('docs/patients.yaml')
+@swag_from('docs/patient.yaml')
 def patients_filter(name):
+    query_patient = db.query(PatientsModel).filter(
+        PatientsModel.FIRST_NAME.ilike(f'%{name.upper()}%')).all()
 
-    print('passei')
-    query_patient = db.query(PatientsModel).filter(PatientsModel.FIRST_NAME.ilike(f'%{name.upper()}%')).all()
-    schema_patient = PatientsSchema(many=True)
-    json_patient = schema_patient.dump(query_patient)
+    json_patient = schema_patients.dump(query_patient)
     status_code = 200
-    result = json_patient
-        
     db.close()
-    return jsonify(result), status_code
+    return jsonify(json_patient), status_code
 
 
 @bp.route('/pharmacies/', methods=['GET'])
-@doc_swagger.swag_from('docs/pharmacies.yaml')
+@swag_from('docs/pharmacies.yaml')
 def pharmacies():
-
     query_pharmacies = db.query(PharmaciesModel).all()
-    json_pharmacies = schema_pharmacies.dump(query_pharmacies)
-
-    return jsonify(json_pharmacies)
-
-@bp.route('/pharmacies/<string:pharmacie>', methods=['GET'])
-@doc_swagger.swag_from('docs/pharmacies.yaml')
-def pharmacies_filter(pharmacie):
-
-    query_pharmacies = db.query(PharmaciesModel).filter(PharmaciesModel.NAME.ilike(f'%{pharmacie.upper()}%')).all()
-    schema_pharmacies = PharmaciesSchema(many=True)
     json_pharmacies = schema_pharmacies.dump(query_pharmacies)
     db.close()
     return jsonify(json_pharmacies)
-    
+
+
+@bp.route('/pharmacies/<string:pharmacie>', methods=['GET'])
+@swag_from('docs/pharmacie.yaml')
+def pharmacies_filter(pharmacie):
+    query_pharmacies = db.query(PharmaciesModel).filter(
+        PharmaciesModel.NAME.ilike(f'%{pharmacie.upper()}%')).all()
+
+    json_pharmacies = schema_pharmacies.dump(query_pharmacies)
+    db.close()
+    return jsonify(json_pharmacies)
 
 
 @bp.route('/transactions/', methods=['GET'])
-@doc_swagger.swag_from('docs/transactions.yaml')
+@swag_from('docs/transactions.yaml')
 def transactions():
-
     query_transactions = db.query(TransactionsModel).all()
     json_transactions = schema_transactions.dump(query_transactions)
-
+    db.close()
     return jsonify(json_transactions)
 
-@bp.route('/transactions/<string:transaction>', methods=['GET'])
-@doc_swagger.swag_from('docs/transaction.yaml')
-def transaction_filter(transaction):
 
-    query_transaction = db.query(TransactionsModel).filter(TransactionsModel.NAME.ilike(f'%{transaction.upper()}%')).all()
-    schema_transaction = TransactionsSchema(many=True)
+@bp.route('/transactions/<string:transaction>', methods=['GET'])
+@swag_from('docs/transaction.yaml')
+def transaction_filter(transaction):
+    query_transaction = db.query(TransactionsModel).filter(
+        TransactionsModel.NAME.ilike(f'%{transaction.upper()}%')).all()
+
     json_transaction = schema_transactions.dump(query_transaction)
     db.close()
     return jsonify(json_transaction)
-
--- pharmacies/pharmacies/ext/database.py
+++ pharmacies/pharmacies/ext/database.py
@@ -6,7 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 PATH_USER = os.getcwd()
-DATABASE_CLOUD = 'postgresql+psycopg2://oyudkrmmcjgyxy:59ffb8e1d788030f6be2ab236f68122255aed7cdd781671f29b42316958f37d9@ec2-54-147-36-107.compute-1.amazonaws.com:5432/dae0nh4a3d6lqp'
 DATABASE_LOCAL = f'sqlite:///{PATH_USER}/pharmacies/ext/backend_test.db'
 
 engine = create_engine(DATABASE_LOCAL)
